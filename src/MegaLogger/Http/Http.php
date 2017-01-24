2d7f5d55bcd5514f9fec013d721837d84b755cbd
DongPhong <bachnx2303@gmail.com>
2017-01-24 11:18:43 +0700

    Update check variable HTTP_USER_AGENT

--- src/MegaLogger/Http/Http.php
+++ src/MegaLogger/Http/Http.php
@@ -31,55 +31,57 @@ class Http {
 
     public function getUserAgent() {
 
-        $userAgent = $_SERVER['HTTP_USER_AGENT'];
         $platform = "N/A";
         $browser = "N/A";
-        $osRegex = array(
-            '/windows nt 10/i' => 'Windows 10',
-            '/windows nt 6.3/i' => 'Windows 8.1',
-            '/windows nt 6.2/i' => 'Windows 8',
-            '/windows nt 6.1/i' => 'Windows 7',
-            '/windows nt 6.0/i' => 'Windows Vista',
-            '/windows nt 5.2/i' => 'Windows Server 2003/XP x64',
-            '/windows nt 5.1/i' => 'Windows XP',
-            '/windows xp/i' => 'Windows XP',
-            '/windows nt 5.0/i' => 'Windows 2000',
-            '/windows me/i' => 'Windows ME',
-            '/win98/i' => 'Windows 98',
-            '/win95/i' => 'Windows 95',
-            '/win16/i' => 'Windows 3.11',
-            '/macintosh|mac os x/i' => 'Mac OS X',
-            '/mac_powerpc/i' => 'Mac OS 9',
-            '/linux/i' => 'Linux',
-            '/ubuntu/i' => 'Ubuntu',
-            '/iphone/i' => 'iPhone',
-            '/ipod/i' => 'iPod',
-            '/ipad/i' => 'iPad',
-            '/android/i' => 'Android',
-            '/blackberry/i' => 'BlackBerry',
-            '/webos/i' => 'Mobile',
-            '/windows phone/i' => 'Windows Phone'
-        );
-        $browserRegex = array(
-            '/MSIE/i' => 'IE',
-            '/Firefox/i' => 'Firefox',
-            '/Chrome/i' => 'Chrome',
-            '/Safari/i' => 'Safari',
-            '/Opera/i' => 'Opera',
-            '/Netscape/i' => 'Netscape'
-        );
+        if(isset($_SERVER['HTTP_USER_AGENT'])){
+            $userAgent = $_SERVER['HTTP_USER_AGENT'];
+            $osRegex = array(
+                '/windows nt 10/i' => 'Windows 10',
+                '/windows nt 6.3/i' => 'Windows 8.1',
+                '/windows nt 6.2/i' => 'Windows 8',
+                '/windows nt 6.1/i' => 'Windows 7',
+                '/windows nt 6.0/i' => 'Windows Vista',
+                '/windows nt 5.2/i' => 'Windows Server 2003/XP x64',
+                '/windows nt 5.1/i' => 'Windows XP',
+                '/windows xp/i' => 'Windows XP',
+                '/windows nt 5.0/i' => 'Windows 2000',
+                '/windows me/i' => 'Windows ME',
+                '/win98/i' => 'Windows 98',
+                '/win95/i' => 'Windows 95',
+                '/win16/i' => 'Windows 3.11',
+                '/macintosh|mac os x/i' => 'Mac OS X',
+                '/mac_powerpc/i' => 'Mac OS 9',
+                '/linux/i' => 'Linux',
+                '/ubuntu/i' => 'Ubuntu',
+                '/iphone/i' => 'iPhone',
+                '/ipod/i' => 'iPod',
+                '/ipad/i' => 'iPad',
+                '/android/i' => 'Android',
+                '/blackberry/i' => 'BlackBerry',
+                '/webos/i' => 'Mobile',
+                '/windows phone/i' => 'Windows Phone'
+            );
+            $browserRegex = array(
+                '/MSIE/i' => 'IE',
+                '/Firefox/i' => 'Firefox',
+                '/Chrome/i' => 'Chrome',
+                '/Safari/i' => 'Safari',
+                '/Opera/i' => 'Opera',
+                '/Netscape/i' => 'Netscape'
+            );
 
-        foreach ($osRegex as $regex => $value) {
+            foreach ($osRegex as $regex => $value) {
 
-            if (preg_match($regex, $userAgent)) {
-                $platform = $value;
+                if (preg_match($regex, $userAgent)) {
+                    $platform = $value;
+                }
             }
-        }
-        foreach ($browserRegex as $regex => $value) {
+            foreach ($browserRegex as $regex => $value) {
 
-            if (preg_match($regex, $userAgent)) {
-                $browser = $value;
-                break;
+                if (preg_match($regex, $userAgent)) {
+                    $browser = $value;
+                    break;
+                }
             }
         }
 
